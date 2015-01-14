2efe9ead44146315d78d106e22d92ba713f7a869
Juan Volpe <juan.volpe@globant.com>
2015-01-14 13:48:12 -0300

    third commit

---
+++ 03-Node-npm-modules/requestHandlers.js
@@ -0,0 +1,21 @@
+var exec = require("child_process").exec;
+
+function iniciar(response) {
+  console.log("Manipulador de petición 'iniciar' fue llamado.");
+
+  exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(stdout);
+    response.end();
+  });
+}
+
+function upload(response) {
+  console.log("Manipulador de petición 'subir' fue llamado.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hola Subir");
+  response.end();
+}
+
+exports.iniciar = iniciar;
+exports.subir = subir;
